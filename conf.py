81f543dac5f5a4f477c3f3a6e598f9139870e17b
Serafeim Zanikolas <serzan@hellug.gr>
2009-05-28 10:07:05 +0200

    rm unusued properties and add comments in conf.py

--- conf.py
+++ conf.py
@@ -1,7 +1,9 @@
 cache_dir = "/home/sez/fun/debian/participation/cache"
 bugs_update_period_in_days = 7
 popcon_update_period_in_days = 30
+# consider packages that are "being adopted" for at least this many days
 being_adopted_threshold_in_days = 150
+# consider packages that are "being packaged" for at least this many days
 being_packaged_threshold_in_days = 150
 
 in_archive = ["orphaned", "help_requested", "being_adopted", "rfa_bypackage"]
@@ -10,5 +12,3 @@ known_bug_types = set(in_archive)
 known_bug_types.update(set(not_in_archive))
 bug_types_to_query = in_archive
 
-orphaned_file = "/home/sez/fun/debian/participation/cache/bugs/orphaned.html"
-popcon_file = "/home/sez/fun/debian/participation/cache/popcon/all-popcon-results.txt"
